232cd51c58724a107218ed9e339353d825e1765f
Mohammed Basim <basimbfk781@gmail.com>
2022-01-30 17:44:46 +0530

    AdviceScreen Initial

--- app/src/main/java/com/example/getanadvice/get_advice_feature/presentation/advicescreen/screen/AdviceScreen.kt
+++ app/src/main/java/com/example/getanadvice/get_advice_feature/presentation/advicescreen/screen/AdviceScreen.kt
@@ -1,8 +1,25 @@
 package com.example.getanadvice.get_advice_feature.presentation.advicescreen.screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.example.getanadvice.R
 
 @Composable
 fun AdviseScreen() {
-
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colors.primary)
+    ) {
+        Text(
+            text = stringResource(id = R.string.home_advise),
+            style = MaterialTheme.typography.h5
+        )
+    }
 }
